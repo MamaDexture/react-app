b331996a565aab20d99769ce9e6f8c60fa992ff1
MamaDexture <dextureosama7@gmail.com>
2024-11-19 16:41:49 +0500

    Counter implementation

---
+++ my-react-app/src/App.js
@@ -0,0 +1,13 @@
+
+import Osama from "./Osama.jsx";
+
+function App() {
+  return (
+    <>
+    <Osama />
+    <h1>Cyber dunya</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/Osama.jsx
@@ -0,0 +1,30 @@
+
+import { useState } from "react";
+
+function Osama() {
+
+  
+let [counter,setCounter] =useState(15);
+const addCounter = () => {
+  if(counter < 30 ){
+    setCounter(counter+1)
+  }
+  
+  }
+  const subtractCounter = () => {
+    if(counter >= 1 ){
+      setCounter(counter-1)
+    }
+    
+  }
+    return (
+      <>
+      <h1>Osama Zafar</h1>
+      <h1>Counuter : {counter}</h1>
+      <button onClick={addCounter}>Add Counter </button>
+      <button onClick={subtractCounter}>Subtract Counter </button>
+      </>
+    );
+  }
+  
+  export default Osama;
